ac5e0abdd96391f01b43be69a095dd4113c0f431
jahir-biz <jahir.uddin@bizcloudexperts.com>
2024-02-28 17:48:45 +0530

    feat: table status checker done

--- src/dell-narvar-pod-doc/helper.js
+++ src/dell-narvar-pod-doc/helper.js
@@ -38,4 +38,29 @@ function getCstTimestamp(date = new Date()) {
   return moment(date).tz('America/Chicago').format('YYYY:MM:DD HH:mm:ss Z');
 }
 
-module.exports = { STATUSES, tableStatuses, publishToSNS, getCstTimestamp };
+const tableParams = {
+  SHIPMENT_MILESTONE_TABLE: ({ orderNo }) => ({
+    TableName: process.env.SHIPMENT_MILESTONE_TABLE,
+    KeyConditionExpression: 'FK_OrderNo = :orderNo and FK_OrderStatusId = :statusId',
+    ExpressionAttributeValues: {
+      ':orderNo': orderNo,
+      ':statusId': 'POD',
+    },
+  }),
+  SHIPPER_TABLE: ({ orderNo }) => ({
+    TableName: process.env.SHIPPER_TABLE,
+    KeyConditionExpression: 'FK_ShipOrderNo = :orderNo',
+    ExpressionAttributeValues: {
+      ':orderNo': orderNo,
+    },
+  }),
+  CONSIGNEE_TABLE: ({ orderNo }) => ({
+    TableName: process.env.CONSIGNEE_TABLE,
+    KeyConditionExpression: 'FK_ConOrderNo = :orderNo',
+    ExpressionAttributeValues: {
+      ':orderNo': orderNo,
+    },
+  }),
+};
+
+module.exports = { STATUSES, tableStatuses, publishToSNS, getCstTimestamp, tableParams };
--- src/dell-narvar-pod-doc/table-status-checker.js
+++ src/dell-narvar-pod-doc/table-status-checker.js
@@ -1,5 +1,148 @@
 'use strict';
 
-module.exports.handler = async (event) => {
+const { get, pickBy } = require('lodash');
+const { STATUSES, tableParams, publishToSNS } = require('./helper');
+const AWS = require('aws-sdk');
+const moment = require('moment-timezone');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+let functionName;
+
+module.exports.handler = async (event, context) => {
+  functionName = context.functionName;
   console.info('event:', JSON.stringify(event));
+  try {
+    const getPendingRecordsRes = await getPendingRecords();
+    console.info(
+      '🙂 -> file: table-status-checker.js:13 -> module.exports.handler= -> getPendingRecordsRes:',
+      getPendingRecordsRes
+    );
+
+    await Promise.all(
+      getPendingRecordsRes.map(async (record) => {
+        const orderNo = get(record, 'FK_OrderId', '');
+        const retryCount = get(record, 'RetryCount', 0);
+
+        const tableStatuses = get(record, 'TableStatuses', {});
+        console.info(
+          '🙂 -> file: table-status-checker.js:17 -> awaitPromise.all -> tableStatuses:',
+          tableStatuses
+        );
+        const originalTableStatuses = { ...tableStatuses };
+
+        const pendingTableStatuses = pickBy(tableStatuses, (value) => value === STATUSES.PENDING);
+        console.info(
+          '🙂 -> file: table-status-checker.js:19 -> awaitPromise.all -> pendingTableStatuses:',
+          pendingTableStatuses
+        );
+        await Promise.all(
+          Object.keys(pendingTableStatuses).map(async (tableName) => {
+            const tableParam = tableParams[tableName]({ orderNo });
+            console.info(
+              '🙂 -> file: table-status-checker.js:25 -> awaitPromise.all -> tableParam:',
+              tableParam
+            );
+            const checkTableDataRes = await checkTableData({ params: tableParam });
+            console.info(
+              '🙂 -> file: table-status-checker.js:27 -> awaitPromise.all -> checkTableDataRes:',
+              checkTableDataRes
+            );
+            originalTableStatuses[tableName] = checkTableDataRes;
+          })
+        );
+        console.info(
+          '🙂 -> file: table-status-checker.js:21 -> awaitPromise.all -> originalTableStatuses:',
+          originalTableStatuses
+        );
+        if (Object.values(originalTableStatuses).includes(STATUSES.PENDING)) {
+          await updateStatusTable({
+            orderNo,
+            originalTableStatuses,
+            retryCount,
+            status: STATUSES.PENDING,
+          });
+        }
+        if (!Object.values(originalTableStatuses).includes(STATUSES.PENDING)) {
+          await updateStatusTable({
+            orderNo,
+            originalTableStatuses,
+            retryCount,
+            status: STATUSES.READY,
+          });
+        }
+        if (Object.values(originalTableStatuses).includes(STATUSES.PENDING) && retryCount >= 5) {
+          await updateStatusTable({
+            orderNo,
+            originalTableStatuses,
+            retryCount,
+            status: STATUSES.FAILED,
+          });
+          throw new Error(`Tables are not populated for Order id: ${orderNo}\n
+          Pending tables: ${JSON.stringify(pickBy(originalTableStatuses, (value) => value === STATUSES.PENDING))}\n
+        `);
+        }
+      })
+    );
+  } catch (error) {
+    const errorMessage = `An error occurred in function ${context.functionName}. Error details: ${error}.`;
+    console.error(errorMessage);
+    await publishToSNS(errorMessage, context.functionName);
+  }
 };
+
+async function getPendingRecords() {
+  const params = {
+    TableName: process.env.DOC_STATUS_TABLE,
+    IndexName: process.env.DOC_STATUS_INDEX,
+    KeyConditionExpression: '#Status = :status',
+    ExpressionAttributeNames: { '#Status': 'Status' },
+    ExpressionAttributeValues: {
+      ':status': STATUSES.PENDING,
+    },
+  };
+  try {
+    const data = await dynamoDB.query(params).promise();
+    return get(data, 'Items', []);
+  } catch (error) {
+    console.error('Validation error:', error);
+    return false;
+  }
+}
+
+async function checkTableData({ params }) {
+  try {
+    const data = await dynamoDB.query(params).promise();
+    return get(data, 'Items', []).length > 0 ? STATUSES.READY : STATUSES.PENDING;
+  } catch (error) {
+    console.error('Validation error:', error);
+    return false;
+  }
+}
+
+async function updateStatusTable({ orderNo, originalTableStatuses, retryCount, status }) {
+  try {
+    const updateParam = {
+      TableName: process.env.DOC_STATUS_TABLE,
+      Key: { FK_OrderId: orderNo },
+      UpdateExpression:
+        'set TableStatuses = :tableStatuses, RetryCount = :retryCount, #Status = :status, LastUpdateBy = :lastUpdateBy, LastUpdatedAt = :lastUpdatedAt',
+      ExpressionAttributeNames: { '#Status': 'Status' },
+      ExpressionAttributeValues: {
+        ':tableStatuses': originalTableStatuses,
+        ':retryCount': retryCount + 1,
+        ':status': status,
+        ':lastUpdateBy': functionName,
+        ':lastUpdatedAt': moment.tz('America/Chicago').format(),
+      },
+    };
+    console.info(
+      '🙂 -> file: table-status-checker.js:79 -> updateStatusTable -> updateParam:',
+      updateParam
+    );
+    return await dynamoDB.update(updateParam).promise();
+  } catch (error) {
+    console.info('🙂 -> file: table-status-checker.js:82 -> updateStatusTable -> error:', error);
+    throw error;
+  }
+}
